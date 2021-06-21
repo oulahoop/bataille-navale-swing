fe86603cb9ee705bbb30eaff2953990e1ac9d4c7
E201780B <nicolas.bervoas@etu.univ-nantes.fr>
2021-06-21 21:11:36 +0200

    squelette de GameManager

---
+++ src/info1/utils/GameManager.java
@@ -0,0 +1,48 @@
+package info1.utils;
+
+import info1.network.Network;
+import info1.ships.Coord;
+
+public class GameManager {
+    Network network;
+
+
+    public GameManager(Network network){ this.network = network; }
+
+    public boolean join(){
+        //TODO
+        return false;
+    }
+
+    public void leave(){
+        //TODO
+
+    }
+
+    public boolean canPlay(){
+        //TODO
+        return false;
+    }
+
+    public boolean shoot(Coord coord){
+        //TODO
+        return false;
+    }
+
+    public boolean gameEnded(){
+        //TODO
+        return false;
+    }
+
+    public boolean won(){
+        //TODO
+        return false;
+    }
+
+
+
+
+
+
+
+}
