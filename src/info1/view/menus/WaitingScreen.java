64e65c5038ef7df31bbfbfa9df35f688be2b164c
E201780B <nicolas.bervoas@etu.univ-nantes.fr>
2021-06-23 17:24:38 +0200

    resize GameMenu

--- src/info1/view/menus/WaitingScreen.java
+++ src/info1/view/menus/WaitingScreen.java
@@ -1,9 +1,12 @@
 package info1.view.menus;
 
+import info1.Application;
+import info1.view.Menu;
 import info1.view.ViewManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.concurrent.CompletableFuture;
 
 public class WaitingScreen {
 
@@ -15,25 +18,34 @@ public class WaitingScreen {
     public WaitingScreen(ViewManager frame) {
         this.viewManager = frame;
 
-        main.setSize(1920,1080);
+        main.setSize(frame.getSize());
         main.setPreferredSize(main.getSize());
         main.setBackground(new Color(0x18384f));
 
         waiting.setDescription("image");
         Image image = waiting.getImage();
-        Image newimg = image.getScaledInstance(main.getWidth()/2, main.getHeight()/4,  java.awt.Image.SCALE_SMOOTH);
+        Image newimg = image.getScaledInstance(main.getWidth() / 2, main.getHeight() / 4, java.awt.Image.SCALE_SMOOTH);
         waiting = new ImageIcon(newimg);
 
         waitingLabel = new JLabel(waiting);
-
         waitingLabel.setVerticalAlignment(JLabel.CENTER);
-
 
         main.add(waitingLabel);
 
         frame.setContentPane(main);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.update();
+
+        CompletableFuture.runAsync(() -> {
+            while(Application.getApp().getGameManager().getGame().getGuest() == null){
+                synchronized(this) {
+                    try {
+                        wait(500);
+                    } catch(InterruptedException ex) { ex.printStackTrace(); }
+                }
+            }
+            viewManager.switchTo(Menu.GAME);
+        });
 
     }
 }
