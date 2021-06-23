628dc9b51b582718a7bf8383dfdd20bfd19d9f00
E200467Z <sylvain.brois@etu.univ-nantes.fr>
2021-06-23 13:32:41 +0200

    le peu de travail réalisé par sylvain ^^

--- src/info1/view/listeners/gameMenu/ListenerTir.java
+++ src/info1/view/listeners/gameMenu/ListenerTir.java
@@ -15,39 +15,70 @@ public class ListenerTir implements ActionListener {
     private GameMenu fenetre;
     private JButton selected;
     private GameManager gameMan;
-    public ListenerTir(GameMenu gm){
+
+    public ListenerTir(GameMenu gm) {
         fenetre = gm;
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        String nom = ((JButton)e.getSource()).getName();
+        String nom = ((JButton) e.getSource()).getName();
 
-        if(nom.equalsIgnoreCase("Tirer")){
-            if(selected != null){
+        if (nom.equalsIgnoreCase("Tirer")) {
+            if (selected != null) {
                 selected.setText("boom");
                 fenetre.activerTir(false);
                 gameMan = Application.getApp().getGameManager();
                 try {
-                    Coord coord  =new Coord(selected.getName());
+                    Coord coord = new Coord(selected.getName());
                     System.out.println(selected.getName());
                     System.out.println(coord.toString());
-                    gameMan.shoot(coord);
-                } catch (BadCoordException badCoordException) {
-                    badCoordException.printStackTrace();
+                    switch (gameMan.shoot(coord)) {
+                        case 0:
+                            miss(coord);
+                            break;
+                        case 1:
+                            hit(coord);
+                            break;
+                        case 10:
+                            sunk(coord);
+                            break;
+                        case 100:
+                            won(coord);
+                            break;}
+
+                    } catch(BadCoordException badCoordException){
+                        badCoordException.printStackTrace();
+                    }
+                }
+            } else {
+                if (((JButton) e.getSource()).getText().equals("")) {
+                    if (selected != null) {
+                        selected.setEnabled(true);
+                        selected.setBackground(new Color(0x78939A));
+                    }
+                    selected = (JButton) e.getSource();
+                    selected.setEnabled(false);
+                    selected.setBackground(new Color(0x253662));
+                    fenetre.activerTir(true);
                 }
             }
         }
-        else{
-            if(((JButton)e.getSource()).getText().equals("")) {
-                if (selected != null) {
-                    selected.setEnabled(true);
-                    selected.setBackground(new Color(0x78939A));
-                }
-                selected = (JButton) e.getSource();
-                selected.setEnabled(false);
-                selected.setBackground(new Color(0x253662));
-                fenetre.activerTir(true);
-            }
-        }
+    }
+
+    private void won(Coord coord) {
+        //TODO
+    }
+
+    private void sunk(Coord coord) {
+        //TODO
+    }
+
+    private void hit(Coord coord) {
+        //TODO
+    }
+
+    private void miss(Coord coord) {
+        //TODO
     }
 }
