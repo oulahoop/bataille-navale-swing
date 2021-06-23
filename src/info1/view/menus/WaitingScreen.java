478c2b14da9c9f45bd20f75938178bf47e431811
E201780B <nicolas.bervoas@etu.univ-nantes.fr>
2021-06-23 15:01:56 +0200

    WaitingScreen creation

--- src/info1/view/menus/WaitingScreen.java
+++ src/info1/view/menus/WaitingScreen.java
@@ -1,4 +1,36 @@
 package info1.view.menus;
 
+import info1.view.ViewManager;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class WaitingScreen {
+
+    private final ViewManager viewManager;
+    ImageIcon waiting = new ImageIcon("src/info1/utils/img/pngFiles/waiting.png");
+    JLabel waitingLabel;
+    JPanel main = new JPanel();
+
+    public WaitingScreen(ViewManager frame) {
+        this.viewManager = frame;
+
+        main.setSize(1920,1080);
+        main.setPreferredSize(main.getSize());
+
+        waiting.setDescription("image");
+
+        waitingLabel = new JLabel(waiting);
+        waitingLabel.setSize(new Dimension(main.getWidth(), main.getHeight()));
+        waitingLabel.setPreferredSize(waitingLabel.getSize());
+
+
+        main.add(waitingLabel);
+
+
+        frame.setContentPane(main);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.update();
+
+    }
 }
