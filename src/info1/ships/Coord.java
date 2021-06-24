f5430fa9c3f26b329269a4235d9866f2bc4c88d5
E201780B <nicolas.bervoas@etu.univ-nantes.fr>
2021-06-24 09:44:14 +0200

    update COORD opti

--- src/info1/ships/Coord.java
+++ src/info1/ships/Coord.java
@@ -1,8 +1,5 @@
 package info1.ships;
 
-
-import java.util.Locale;
-
 /**
  * une implementation de l'interface ICoord manipulant des coordonnées alphanumériques comme "A1", "B6", "J3", etc.
  */
@@ -18,69 +15,40 @@ public class Coord implements ICoord {
      * @throws BadCoordException si la chaine de caractère ne permet pas de définir une coordonnée alphanumérique
      */
     public Coord(String xy) throws BadCoordException {
-
         if(xy.length() > 3 || xy.length()<2) throw new BadCoordException();
+        xy = xy.toUpperCase();
 
-        char letter = xy.toLowerCase(Locale.ROOT).charAt(0);
+        char letter = xy.charAt(0);
         boolean valid = false;
-
-        for (int i = 97; i < 107; i++) {
+        for (int i = 65; i < 75; i++) {
             if(letter == (char)i ) valid = true;
         }
         if (!valid) throw new BadCoordException();
 
-        if(xy.length() ==3){
-            if( Integer.parseInt(String.valueOf(xy.charAt(1)))*10 + Integer.parseInt(String.valueOf(xy.charAt(2))) != 10) throw new BadCoordException();
-        }
+        boolean length = (xy.length() == 3);
+        int number = length ? Integer.parseInt(xy.charAt(1) +""+ xy.charAt(2)) : Integer.parseInt(String.valueOf(xy.charAt(1)));
+        if(number < 1 || number > 10) throw new BadCoordException();
 
-        if(xy.length() == 2){
-            valid =false;
-            for (int i = 1; i < 10; i++) {
-                if(Integer.parseInt(String.valueOf(xy.charAt(1))) == i) valid = true;
-            }
-            if (!valid) throw new BadCoordException();
-        }
         this.xy = xy;
-
-
     }
 
     public char getAlphaX() {
         return xy.charAt(0);
     }
 
-    public int getX() {
-        int compt = 1;
-        char letter = xy.toLowerCase(Locale.ROOT).charAt(0);
-        for (int i = 97; i < 107; i++) {
-            if(letter == (char)i ) return compt;
-            compt++;
-        }
-        return -1;
-    }
+    public int getX() { return (int)xy.charAt(0) - 64; }
 
     public int getY() {
-        if(xy.length() == 3){
-            return Integer.parseInt(String.valueOf(xy.charAt(1)))*10 + Integer.parseInt(String.valueOf(xy.charAt(2)));
-        }
-        return Integer.parseInt(String.valueOf(xy.charAt(1)));
+        boolean length = (xy.length() == 3);
+        return length ? Integer.parseInt(xy.charAt(1) +""+ xy.charAt(2)) : Integer.parseInt(String.valueOf(xy.charAt(1)));
     }
 
-    public String toString() {
+    public String toString() { return xy; }
 
-        return xy.toUpperCase(Locale.ROOT);
-    }
-
-    public boolean equals(Object o) {
-
-        return o instanceof Coord && ((Coord) o).getX() == this.getX() && ((Coord) o).getY() == this.getY();
-    }
+    public boolean equals(Object o) { return o instanceof Coord && ((Coord)o).hashCode() == this.hashCode(); }
 
     public int hashCode() {
-        StringBuilder sb = new StringBuilder();
-        if(xy.length()>2)sb.append((int)xy.charAt(0)).append(Integer.parseInt(String.valueOf(xy.charAt(1)))).append(Integer.parseInt(String.valueOf(xy.charAt(2))));
-        else sb.append((int)xy.charAt(0)).append(Integer.parseInt(String.valueOf(xy.charAt(1))));
-        int hashcode = Integer.parseInt(sb.toString());
-        return hashcode ;
+        boolean length = (xy.length() == 3);
+        return (int)xy.charAt(0) + (length ? Integer.parseInt(xy.charAt(1) +""+ xy.charAt(2)) : Integer.parseInt(String.valueOf(xy.charAt(1))));
     }
 }
